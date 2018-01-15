a2c3455c89e2f4723a8eeb2f2dfd1bb1478b0923
eric <eric.pozo@sfr.fr>
2018-01-15 16:58:00 +0100

    nouvelle fonction clear

--- app/src/main/java/fr/m2i/signature/MainActivity.java
+++ app/src/main/java/fr/m2i/signature/MainActivity.java
@@ -1,13 +1,19 @@
 package fr.m2i.signature;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.accessibility.AccessibilityManager;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
     private  TagEdit teSignature;
+    private TagEdit teSignature2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,16 +21,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         teSignature = findViewById(R.id.teSignature);
-        teSignature.setTagColor(Color.BLACK); //force la couleur du crayon
+        teSignature.setTagColor(Color.CYAN); //force la couleur du crayon
         teSignature.setTagWidth(20); // force la taille du crayon
 
+        teSignature2 = findViewById(R.id.teSignature2);
+        teSignature2.setTagColor(Color.RED);
+        teSignature2.setTagWidth(10);
     }
-    public void btClear(View v){
+
+    public void btClear1(View v){
 
         teSignature.clear();
 
             }
+    public void btClear2(View v) throws IOException {
 
+        teSignature2.save("test.png");
+
+    }
 
 
 
